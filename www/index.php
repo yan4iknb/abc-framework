b4957e4b815d3adea6d42d3c83971ff9c77b6169
abc-framework <abc-framework@mail.ru>
2015-10-14 13:04:02 +0700

    edit

--- www/index.php
+++ www/index.php
@@ -2,12 +2,16 @@
 
 namespace ABC;
 
-    require_once __DIR__ .'/../vendor/abc/abc.php'; 
-    $config = require_once __DIR__ .'/../versions/app/resourses/config.php';
+    require __DIR__ .'/../vendor/abc/abc.php'; 
+    $config = require __DIR__ .'/../versions/app/resourses/config.php';
    
     Abc::createNew($config);
-    //echo Abc::current()->install();
-    echo Abc::getVersion('debuggers');
+    
+    // Так будет запускаться инсталлятор.
+    echo Abc::current()->install();
+    
+    
+
     
     
     

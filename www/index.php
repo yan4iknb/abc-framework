8cde09a09c0ba55335799da62ee1fbc09f5b3750
abc-framework <abc-framework@mail.ru>
2015-10-30 21:29:49 +0700

    Update index.php

--- www/index.php
+++ www/index.php
@@ -3,14 +3,10 @@
 namespace ABC;
 
     error_reporting(E_ALL);
-    
-    require __DIR__ .'/../vendor/abc/Abc.php';
-    //require __DIR__ .'/../vendor/abc/core/Autoloader.php'; 
-    
+    require __DIR__ .'/../vendor/Abc/Abc.php';  
     $config = require __DIR__ .'/../app/resourses/config.php';
    
     Abc::createApp($config);
-    //(new \ABC\abc\core\AbcProcessor($config))->route();
 
     
     

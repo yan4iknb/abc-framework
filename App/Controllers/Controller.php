0bad3d4353db4fa8fcf26c0f7deb44a0e37fe5ae
abc-framework <abc-framework@mail.ru>
2015-10-30 23:22:26 +0700

    app

---
+++ App/Controllers/Controller.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace ABC\App\Controllers;
+
+use ABC\Abc;
+use ABC\Abc\Core\BaseController;
+use ABC\App\Example;
+
+/** 
+ * Контролер
+ * 
+ * NOTE: Requires PHP version 5.5 or later   
+ * @author phpforum.su
+ * @copyright © 2015
+ * @license http://abc-framework.com/license/ 
+ */   
+class Controller extends BaseController
+{ 
+    public function __construct()
+    {
+        //Abc::getService('Traning')->display();    
+        new Example; 
+    }
+}
+
+
+
+
+
+
+
+
+
+

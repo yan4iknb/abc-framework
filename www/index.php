cff833e01bd09a71f01a678cb8e12d91dd7310e1
abc-framework <abc-framework@mail.ru>
2016-04-21 01:40:27 +0700

    index

--- www/index.php
+++ www/index.php
@@ -2,10 +2,8 @@
 
 namespace ABC;
 
-
     require __DIR__ .'/../vendor/Abc/Abc.php';
-    $local  = require __DIR__ .'/configs/local.php'; 
-  
+    $local  = require __DIR__ .'/configs/local.php';   
     Abc::startApp($local);
 
  

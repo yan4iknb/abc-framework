58220e2c909c0e9eb4e26a2ebcb0a3dfb775a544
abc-framework <abc-framework@mail.ru>
2016-06-28 23:16:10 +0700

    config

--- www/configs/local.php
+++ www/configs/local.php
@@ -1,5 +1,12 @@
 <?php
 
-    return [    
-                'abc_debug'        => true,  
+    return [   
+    
+                'abc_debug'        => true, 
+                
+                'settings'  => [    
+                                    'dir_template'    => dirname(dirname(__DIR__))  
+                                                   .'/'. basename(dirname(__DIR__)) 
+                                                   .'/theme/tpl/', 
+                ], 
     ];
